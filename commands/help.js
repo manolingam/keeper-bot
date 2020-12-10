fd99ba52b18b1cfc1f27d3162c88cd107820ded2
Manolingam N <manolingam1996@gmail.com>
2020-12-10 11:59:44 +0530

    new availablility commands & invocation update

--- commands/help.js
+++ commands/help.js
@@ -1,91 +1,129 @@
 module.exports = {
-    name: "help",
-    description: "Returns the list of available commands for use.",
-    execute(Discord, message, args) {
-        const CREATE_RAID = `To create a raid channel, use **!keeper create-raid <project-name> <link-to-proposal> <party-member-1> <party-member-2>**. You don't need to mention yourself as a party member as it's default.`;
-        const CREATE_RIP = `To create a rip channel, use **!keeper create-rip <project-name> <link-to-proposal> <party-member-1> <party-member-2>**. You don't need to mention yourself as a party member as it's default.`;
-        const CREATE_CAMP = `To create a camp channel, use **!keeper create-camp <project-name> <link-to-proposal> <party-member-1> <party-member-2>**. You don't need to mention yourself as a party member as it's default.`;
-        const VALHALLA = `To send a channel to Valhalla, use **!keeper valhalla <mention channel>**.`;
-        const ROLE_STATS = `Returns the total number of members assigned to each role.`;
-        const INACTIVE_STATS = `Returns total inactive members & their usernames.`;
-        const TREASURY = `To record a direct transfer into the DAO bank, use **!keeper treasury "brief description" etherscan-link**.`;
-        const GAS_INFO = `Returns the live gas price stats.`;
-        const TIMEZONES = `Returns the current time in different timezones.`;
+  name: 'help',
+  description: 'Returns the list of available commands for use.',
+  execute(Discord, message, args) {
+    const CREATE_RAID = `To create a raid channel, use **!keeper create-raid <project-name> <link-to-proposal> <party-member-1> <party-member-2>**. You don't need to mention yourself as a party member as it's default.`;
+    const CREATE_RIP = `To create a rip channel, use **!keeper create-rip <project-name> <link-to-proposal> <party-member-1> <party-member-2>**. You don't need to mention yourself as a party member as it's default.`;
+    const CREATE_CAMP = `To create a camp channel, use **!keeper create-camp <project-name> <link-to-proposal> <party-member-1> <party-member-2>**. You don't need to mention yourself as a party member as it's default.`;
+    const AVAILABLE =
+      "Sets member's own availability for raids. Use `!keeper available <true> |OR| <false>`.";
+    const AVAILABLE_STATUS =
+      "Checks and displays member's availability. Use `!keeper availability-status` ";
+    const AVAILABLE_WHO =
+      'Searches the guild for all **available** members by role(s). Use `!keeper available-who <TAGNAME(S)>`. Can handle multiple queries. Case insensitive. Searches for role (ex. Paladin) OR profession (BackEndDev). Does not return unavailable members.';
+    const AVAILABLE_GREP =
+      'Searches for available guild members by role with a substring. Use `!keeper available-grep <SUBSTRING>`. Only searches one substring. Does not return unavailble members.';
+    const AVAILABLE_FULL =
+      'Searches the guild for all **available** and **unavailable** members by role(s). Use `!keeper available-full <TAGNAME(S)>`. Can handle multiple queries. Case insensitive. Searches for Role (ex. Paladin) OR profession (BackEndDev).';
+    const VALHALLA = `To send a channel to Valhalla, use **!keeper valhalla <mention channel>**.`;
+    const ROLE_STATS = `Returns the total number of members assigned to each role.`;
+    const INACTIVE_STATS = `Returns total inactive members & their usernames.`;
+    const TREASURY = `To record a direct transfer into the DAO bank, use **!keeper treasury "brief description" etherscan-link**.`;
+    const GAS_INFO = `Returns the live gas price stats.`;
+    const TIMEZONES = `Returns the current time in different timezones.`;
 
-        if (args.length < 3)
-            return message.channel.send("Missing command! Help with what?");
+    if (args.length < 3)
+      return message.channel.send('Missing command! Help with what?');
 
-        switch (args[2]) {
-            case "create-raid":
-                return message.channel.send(
-                    new Discord.MessageEmbed()
+    switch (args[2]) {
+      case 'create-raid':
+        return message.channel.send(
+          new Discord.MessageEmbed()
 
-                        .setDescription(CREATE_RAID)
-                        .setColor("#ff3864")
-                );
-            case "create-rip":
-                return message.channel.send(
-                    new Discord.MessageEmbed()
+            .setDescription(CREATE_RAID)
+            .setColor('#ff3864')
+        );
+      case 'create-rip':
+        return message.channel.send(
+          new Discord.MessageEmbed()
 
-                        .setDescription(CREATE_RIP)
-                        .setColor("#ff3864")
-                );
-            case "create-camp":
-                return message.channel.send(
-                    new Discord.MessageEmbed()
+            .setDescription(CREATE_RIP)
+            .setColor('#ff3864')
+        );
+      case 'create-camp':
+        return message.channel.send(
+          new Discord.MessageEmbed()
 
-                        .setDescription(CREATE_CAMP)
-                        .setColor("#ff3864")
-                );
-            case "valhalla":
-                return message.channel.send(
-                    new Discord.MessageEmbed()
+            .setDescription(CREATE_CAMP)
+            .setColor('#ff3864')
+        );
+      case 'available':
+        return message.channel.send(
+          new Discord.MessageEmbed()
 
-                        .setDescription(VALHALLA)
-                        .setColor("#ff3864")
-                );
-            case "role-stats":
-                return message.channel.send(
-                    new Discord.MessageEmbed()
+            .setDescription(AVAILABLE)
+            .setColor('#ff3864')
+        );
+      case 'available-status':
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setDescription(AVAILABLE_STATUS)
+            .setColor('#ff3864')
+        );
+      case 'available-who':
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setDescription(AVAILABLE_WHO)
+            .setColor('#ff3864')
+        );
+      case 'available-grep':
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setDescription(AVAILABLE_GREP)
+            .setColor('#ff3864')
+        );
+      case 'available-full':
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setDescription(AVAILABLE_FULL)
+            .setColor('#ff3864')
+        );
+      case 'valhalla':
+        return message.channel.send(
+          new Discord.MessageEmbed()
 
-                        .setDescription(ROLE_STATS)
-                        .setColor("#ff3864")
-                );
-            case "inactive-stats":
-                return message.channel.send(
-                    new Discord.MessageEmbed()
+            .setDescription(VALHALLA)
+            .setColor('#ff3864')
+        );
+      case 'role-stats':
+        return message.channel.send(
+          new Discord.MessageEmbed()
 
-                        .setDescription(INACTIVE_STATS)
-                        .setColor("#ff3864")
-                );
-            case "treasury":
-                return message.channel.send(
-                    new Discord.MessageEmbed()
+            .setDescription(ROLE_STATS)
+            .setColor('#ff3864')
+        );
+      case 'inactive-stats':
+        return message.channel.send(
+          new Discord.MessageEmbed()
 
-                        .setDescription(TREASURY)
-                        .setColor("#ff3864")
-                        .setFooter(
-                            'Quotation marks "" are required enclosing the description.'
-                        )
-                );
-            case "gas-info":
-                return message.channel.send(
-                    new Discord.MessageEmbed()
-
-                        .setDescription(GAS_INFO)
-                        .setColor("#ff3864")
-                );
-            case "timezones":
-                return message.channel.send(
-                    new Discord.MessageEmbed()
-
-                        .setDescription(TIMEZONES)
-                        .setColor("#ff3864")
-                );
-            default:
-                return message.channel.send(
-                    "Hmm.. I don't know what that command does. Check `!keeper help`"
-                );
-        }
-    },
+            .setDescription(INACTIVE_STATS)
+            .setColor('#ff3864')
+        );
+      case 'treasury':
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setDescription(TREASURY)
+            .setColor('#ff3864')
+            .setFooter(
+              'Quotation marks "" are required enclosing the description.'
+            )
+        );
+      case 'gas-info':
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setDescription(GAS_INFO)
+            .setColor('#ff3864')
+        );
+      case 'timezones':
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setDescription(TIMEZONES)
+            .setColor('#ff3864')
+        );
+      default:
+        return message.channel.send(
+          "Hmm.. I don't know what that command does. Check `!keeper help`"
+        );
+    }
+  }
 };
--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const TWITTER_ROUTER = require("./routes/twitter");
 const ESCROW_ROUTER = require("./routes/escrow");
 
 Airtable.configure({
-    endpointUrl: "https://api.airtable.com",
-    apiKey: process.env.API_KEY,
+  endpointUrl: "https://api.airtable.com",
+  apiKey: process.env.API_KEY,
 });
 
 let daoshop_base = Airtable.base(process.env.DAOSHOP_BASE_ID);
@@ -25,60 +25,60 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(
-    "/payload",
-    (req, res, next) => {
-        req.DISCORD = Discord;
-        req.CLIENT = client;
-        next();
-    },
-    PAYLOAD_ROUTER
+  "/payload",
+  (req, res, next) => {
+    req.DISCORD = Discord;
+    req.CLIENT = client;
+    next();
+  },
+  PAYLOAD_ROUTER
 );
 app.use(
-    "/daoshop",
-    (req, res, next) => {
-        req.DISCORD = Discord;
-        req.CLIENT = client;
-        req.DAOSHOP_BASE = daoshop_base;
-        if (req.body.key === process.env.ROUTE_ACCESS_KEY) {
-            next();
-        } else {
-            return res.send("Unauthorized access!");
-        }
-    },
-    DAOSHOP_ROUTER
+  "/daoshop",
+  (req, res, next) => {
+    req.DISCORD = Discord;
+    req.CLIENT = client;
+    req.DAOSHOP_BASE = daoshop_base;
+    if (req.body.key === process.env.ROUTE_ACCESS_KEY) {
+      next();
+    } else {
+      return res.send("Unauthorized access!");
+    }
+  },
+  DAOSHOP_ROUTER
 );
 app.use(
-    "/hireus",
-    (req, res, next) => {
-        req.DISCORD = Discord;
-        req.CLIENT = client;
-        req.RAID_CENTRAL_V2_BASE = raid_central_v2_base;
-        if (req.body.key === process.env.ROUTE_ACCESS_KEY) {
-            next();
-        } else {
-            return res.send("Unauthorized access!");
-        }
-    },
-    HIREUS_ROUTER
+  "/hireus",
+  (req, res, next) => {
+    req.DISCORD = Discord;
+    req.CLIENT = client;
+    req.RAID_CENTRAL_V2_BASE = raid_central_v2_base;
+    if (req.body.key === process.env.ROUTE_ACCESS_KEY) {
+      next();
+    } else {
+      return res.send("Unauthorized access!");
+    }
+  },
+  HIREUS_ROUTER
 );
 app.use(
-    "/escrow",
-    (req, res, next) => {
-        req.RAID_CENTRAL_V2_BASE = raid_central_v2_base;
-        next();
-    },
-    ESCROW_ROUTER
+  "/escrow",
+  (req, res, next) => {
+    req.RAID_CENTRAL_V2_BASE = raid_central_v2_base;
+    next();
+  },
+  ESCROW_ROUTER
 );
 app.use(
-    "/twitter",
-    (req, res, next) => {
-        req.CLIENT = client;
-        next();
-    },
-    TWITTER_ROUTER
+  "/twitter",
+  (req, res, next) => {
+    req.CLIENT = client;
+    next();
+  },
+  TWITTER_ROUTER
 );
 app.get("/", (req, res) => {
-    res.send("Hi");
+  res.send("Hi");
 });
 
 client.login(process.env.TOKEN);
@@ -86,9 +86,9 @@ client.login(process.env.TOKEN);
 app.listen(process.env.PORT || 5000, () => console.log("Listening.."));
 
 mongoose.connect(
-    process.env.MONGODB_CONNECTION,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-        console.log("Connected to database..");
-    }
+  process.env.MONGODB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to database..");
+  }
 );

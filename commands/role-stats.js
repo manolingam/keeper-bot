a2f94deb1bf662434acd00bdfabd6b13ebb23955
Manolingam N <manolingam1996@gmail.com>
2021-05-19 13:52:12 +0530

    formatting in role stats

--- commands/role-stats.js
+++ commands/role-stats.js
@@ -8,19 +8,43 @@ module.exports = {
   callback: ({ channel }) => {
     try {
       let roles = [];
-      let filterRoles = ['@everyone'];
+      let filterRoles = [
+        '@everyone',
+        'Available',
+        'xDai-Faucet',
+        'suggestion_bot',
+        'sesh',
+        'Friend Time',
+        'Collab.Land',
+        'Keeper',
+        'HausBOT',
+        'Server Booster',
+        'INACTIVE',
+        'verified',
+        'Available',
+        'BrightID Bot',
+        'RaidGuild SourceCred',
+        'xDai-Faucet',
+        'Friend Time',
+        'sesh'
+      ];
+
+      let ignoredRoles = '';
+      filterRoles.map((role) => (ignoredRoles += `__*${role}*__ `));
 
       channel.guild.roles.cache.forEach((role) => {
         if (!filterRoles.includes(role.name)) {
           let count = channel.guild.roles.cache.get(role.id).members.size;
-          roles.push({ name: role.name, value: `${count}` });
+          roles.push(`**${role.name}** - ${count}`);
         }
       });
 
       let embed = new MessageEmbed()
         .setColor('#ff3864')
-        .setTimestamp()
-        .addFields(roles);
+        .setDescription(
+          `Counted in the important roles while ignoring these.\n\n${ignoredRoles}`
+        )
+        .addFields({ name: 'Counted roles', value: roles });
 
       return embed;
     } catch (err) {

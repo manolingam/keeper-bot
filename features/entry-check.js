992cb1ee694713267abf5a7ef485a04704247a47
Manolingam N <manolingam1996@gmail.com>
2021-05-21 16:01:25 +0530

    added control over entry check

---
+++ features/entry-check.js
@@ -0,0 +1,25 @@
+const { welcomeMessages } = require('../utils/helpers');
+
+const entryCheck = (client, portcullis) => {
+  client.on('guildMemberAdd', (member) => {
+    const tavern = member.guild.channels.cache.get(
+      process.env.TAVERN_CHANNEL_ID
+    );
+    const commandCenter = member.guild.channels.cache.get(
+      process.env.COMMAND_CENTER_ID
+    );
+
+    if (member.user.bot && portcullis) {
+      commandCenter.send(`Kicked unauthorized bot, <@${member.id}>`);
+      member.kick();
+    } else if (member.user.bot && !portcullis) {
+      tavern.send(
+        `This bot is allowed to stay. Prove your worth, <@${member.id}>`
+      );
+    } else {
+      tavern.send(welcomeMessages(member));
+    }
+  });
+};
+
+exports.entryCheck = entryCheck;

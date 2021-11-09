fa74ea5f6cf9aa5f2cf6d16571482e4c6fa7ca7a
Manolingam N <manolingam1996@gmail.com>
2021-11-09 18:59:21 +0530

    new create invite command

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const client = new Client({
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.DIRECT_MESSAGES
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.GUILD_INVITES
   ]
 });
 
